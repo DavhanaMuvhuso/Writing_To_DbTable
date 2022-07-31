fcc33e61832ab9ab2503af017fe866e825a6bfc5
Kingdom Davhana <79306442+DavhanaMuvhuso@users.noreply.github.com>
2022-07-31 23:16:47 +0200

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Title="Home Page" Language="VB" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.vb" Inherits="Writting_to_database._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <div id="lbl_txt">
+            <asp:Label ID="Label1" runat="server" Text="Enter First Name"></asp:Label>
+            <br />
+            <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Enter Gender"></asp:Label>
+            <br />
+            <asp:TextBox ID="txtGender" runat="server"></asp:TextBox>
+        </div>
+        <div id="btn">
+            <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick ="btnSubmit_Click" />
+
+        </div>
+
+
+    </div>
+
+</asp:Content>
---
+++ Default.aspx.vb
@@ -0,0 +1,34 @@
+﻿Imports System.Data.SqlClient
+Imports System.Data.Sql
+Public Class _Default
+
+    Inherits Page
+    'Create ADO.NET Objacts
+    Private myConn As SqlConnection
+    Private myCmd As SqlCommand
+    Private myReader As SqlDataReader
+    Private Results As String
+
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+        'Store Id Ref in Session
+        Session("IdNum") = 14
+
+    End Sub
+
+    Protected Sub btnSubmit_Click(sender As Object, e As EventArgs)
+        'Connection string
+        myConn = New SqlConnection("Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=" & "C:\USERS\KINGD\DESKTOP\DOCS\ZIP\QUESTION 3\QUESTION 3\APP_DATA\QUESTION3DB.MDF" & ";Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False")
+
+        'Create command Object
+        Dim strSql As String = "Insert into testing (Id,firstName, Gender) Values ( " & Session("IdNum") & ", '" & txtName.Text & "', '" & txtGender.Text & "' )"
+        myCmd = myConn.CreateCommand
+        myCmd.CommandText = strSql
+
+        'Open the connection
+        myConn.Open()
+
+        myCmd.ExecuteNonQuery()
+
+    End Sub
+End Class
